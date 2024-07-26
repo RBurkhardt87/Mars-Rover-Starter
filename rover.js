7d83f8167b9878283d822c64c82239d3b7454b4a
Rachel Burkhardt <r.burkhardt.ibclc@gamil.com>
2024-07-26 16:36:10 -0500

    test 10-12 work now, but 9 and 8 are messed up. I think I need to work on my conditionals

--- rover.js
+++ rover.js
@@ -13,7 +13,9 @@ class Rover {
     receiveMessage(message) {
       let statusCheckArray = [];
       let statusCheckObject = {};
-      let otherCommandObject = {};
+      let stats = {};
+
+      //CHECK FOR:  STATUS CHECK
       
       for (let i = 0; i < message.commands.length; i++) { 
          if (message.commands[i].commandType === 'STATUS_CHECK') {          
@@ -22,46 +24,72 @@ class Rover {
             roverStatus : {mode : this.mode, generatorWatts : this.generatorWatts, position : this.position}
          };
             statusCheckArray.push(statusCheckObject);
-                                                            //ideally, I should be able to make if statements for the other
-            let stats = {                                   //commands. they will have their own object response that I can
-               message : message.name,                      //push into statusCheckArray and return the stats object. Once that
-               results : statusCheckArray                   // is done. I probably will no longer need the status object. I would
-            }                                               //think I would just have validation for invalid command if anything.
-            return stats;
+                                                            
+       
          } 
 
-         if (message.commands[i].commandType === 'MODE_CHANGE', 'NORMAL') {
-            statusCheckObject = {
-               complete : true
-            };
-               statusCheckArray.push(statusCheckObject);
 
-               let stats = {
+         //CHECK FOR MODE VALUES: NORMAL and LOW POWER
+
+
+         for (let i = 0; i < message.commands.length; i++){
+            if (message.commands[i].value === 'NORMAL'){
+               statusCheckObject = {
+               complete : true 
+            } 
+            statusCheckArray.push(statusCheckObject);
+
+         }
+
+
+            if (message.commands[i].value === 'LOW_POWER') {
+               statusCheckObject = {
+               complete : false
+               };
+               statusCheckArray.push(statusCheckObject);
+            }
+              
+            
+         }   
+               
+         
+               stats = {
                   message : message.name,
                   results : statusCheckArray
                }
                return stats;
-         }
-     
-         }        
-         let status = {
+            };
+
+         
+         
+
+         
+      
+         //CHECK FOR BASIC COMMAND RETURNS UNTIL ALL CONDITIONS ARE WRITTEN   
+         stats = {
          message : message.name,
-         results : message.commands                          //message.commands (makes test 9 work), but I feel like I have to change it in order
-         }                                                  // to get the real commands to work. When I have it as statusCheckArray I feel like I 
-                                                            // am so close to having it. 
-      return status;
-      }
+         results : message.commands                          
+                                                          
+         }
+                                                                                                               
+      return stats;
+            }
+      
 
 };
 
+
+
+
 // let commands = [new Command('not status check')];
-// let commands = [new Command('STATUS_CHECK')];
+let commands = [new Command('STATUS_CHECK')];
 // let commands = [new Command('MODE_CHANGE', 'NORMAL')]
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER')]
+// let commands = [new Command('MODE_CHANGE', 'LOW_POWER')]
 let message = new Message('Test message with two commands', commands);
 let rover = new Rover(100000)  
 let response = rover.receiveMessage(message);
-console.log(commands[0].value);           //<----------That's the VALUE that we need to check for LOW_POWER MODE!!!!!!!!!!!!
+console.log(response.results);
+// console.log(commands[0].value);           //<----------That's the VALUE that we need to check for LOW_POWER MODE!!!!!!!!!!!!
 // console.log('this is receiveMessage results', response.results);
 // console.log("\n\n");
 // console.log('this is what message.commands looks like', message.commands);
